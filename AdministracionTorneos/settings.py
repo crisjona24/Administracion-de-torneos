4312d1a4dc317639233cf5cbbf5fb9290f677918
Cristobal Jonathan Rios <78310047+crisjona24@users.noreply.github.com>
2022-02-14 09:58:37 -0500

    Cambio de settings

--- AdministracionTorneos/settings.py
+++ AdministracionTorneos/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS= (os.path.join(BASE_DIR,'AdministracionTorneos/static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'AdministracionTorneos/static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'AdministracionTorneos/static') # MODIFICACION DEL STATIC ROOT
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
